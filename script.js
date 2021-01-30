9c2ccb743355fbb2bb216a80d77a96920ca36666
Ravord <ravord2@gmail.com>
2021-01-30 18:43:37 +0100

    version 1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+let inputs = document.querySelectorAll('input');
+let gifs = document.querySelector('.gifs');
+inputs[1].addEventListener('click', () => {
+  $.get(
+    `http://api.giphy.com/v1/gifs/search?q=${inputs[0].value}&api_key=DC0AenRuwr3bYszCS8hV90K09wN4iUmZ`
+  ).then((response) => {
+    $.each(response.data, function (elem) {
+      gifs.innerHTML += `<img src='${response.data[elem].images.original.url}' width='20%' />`;
+    });
+    inputs[0].value = '';
+  });
+});
+inputs[2].addEventListener('click', () => {
+  gifs.innerHTML = '';
+  inputs[0].value = '';
+});
