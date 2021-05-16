e9084a7da79414cea06738cf962ac18661f6b3c0
m_skotarek <76743591+Ravord@users.noreply.github.com>
2021-05-16 22:40:28 +0200

    version 1.1: fixed one issue

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let inputs = document.querySelectorAll('input');
 let gifs = document.querySelector('.gifs');
 inputs[1].addEventListener('click', () => {
   $.get(
-    `http://api.giphy.com/v1/gifs/search?q=${inputs[0].value}&api_key=DC0AenRuwr3bYszCS8hV90K09wN4iUmZ`
+    `https://api.giphy.com/v1/gifs/search?q=${inputs[0].value}&api_key=DC0AenRuwr3bYszCS8hV90K09wN4iUmZ`
   ).then((response) => {
     $.each(response.data, function (elem) {
       gifs.innerHTML += `<img src='${response.data[elem].images.original.url}' width='20%' />`;
