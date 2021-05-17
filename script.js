bde70bf16ddb0bcb2bef170305354e65341ae101
Ravord <ravord2@gmail.com>
2021-05-17 20:09:13 +0200

    version 1.2: switched to axios from jquery ajax

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-let inputs = document.querySelectorAll('input');
-let gifs = document.querySelector('.gifs');
-inputs[1].addEventListener('click', () => {
-  $.get(
-    `https://api.giphy.com/v1/gifs/search?q=${inputs[0].value}&api_key=DC0AenRuwr3bYszCS8hV90K09wN4iUmZ`
-  ).then((response) => {
-    $.each(response.data, function (elem) {
-      gifs.innerHTML += `<img src='${response.data[elem].images.original.url}' width='20%' />`;
-    });
-    inputs[0].value = '';
-  });
-});
-inputs[2].addEventListener('click', () => {
-  gifs.innerHTML = '';
-  inputs[0].value = '';
-});
+let form = document.querySelector('form')
+let inputFind = document.querySelector('input[type="text"]')
+let inputDelete = document.querySelector('input[type="button"]')
+let gifs = document.querySelector('.gifs')
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  gifs.innerHTML = ''
+  axios.get(`https://api.giphy.com/v1/gifs/search?q=${inputFind.value}&api_key=DC0AenRuwr3bYszCS8hV90K09wN4iUmZ`)
+    .then((response) => {
+      response.data.data.map((elem, id) => {
+        gifs.innerHTML += `<img src="${response.data.data[elem, id].images.original.url}" width="20%" />`
+      })
+    })
+  inputFind.value = ''
+})
+inputDelete.addEventListener('click', () => {
+  gifs.innerHTML = ''
+  inputFind.value = ''
+})
